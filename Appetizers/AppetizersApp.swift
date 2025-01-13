6f669ed96f753ed7f75cf4c4f98c0422ff6bc1d4
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-13 15:23:11 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Анастасія Грисюк on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
