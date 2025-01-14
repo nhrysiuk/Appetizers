fa4e8c9f4b3d00589c5c2a586996ebbba06a437a
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-14 21:51:05 +0200

    Add navigation to Detail View

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -13,42 +13,46 @@ struct AppetizerListView: View {
     
     var body: some View {
         ZStack {
-            NavigationView {
-                List(viewModel.appetizers) { appetizer in
-                    AppetizerView(appetizer: appetizer)
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(viewModel.appetizers) { appetizer in
+                       AppetizerView(appetizer: appetizer)
+                            .onTapGesture {
+                                viewModel.chosenAppetizer = appetizer
+                                viewModel.isShowingDetail = true
+                            }
+                    }
                 }
-                .navigationTitle("🥗 Appetizers")
+                .padding()
+                .onAppear() { viewModel.getAppetizers() }
+                
+                .alert(
+                    viewModel.alertItem?.title ?? "Error",
+                    isPresented: $viewModel.alertIsPresented
+                ) {
+                } message: {
+                    Text(
+                        viewModel.alertItem?.message ?? ""
+                    )
+                }
             }
-            .onAppear() { viewModel.getAppetizers() }
-            
-            .alert(
-                viewModel.alertItem?.title ?? "Error",
-                isPresented: $viewModel.alertIsPresented
-            ) {
-            } message: {
-                Text(
-                    viewModel.alertItem?.message ?? ""
-                )
-            }
-            
             .blur(radius: viewModel.isShowingDetail ? 10 : 0)
             if viewModel.isShowingDetail {
-                AppetizerDetailView(appetizer: MockData.appetizer)
+                AppetizerDetailView(appetizer: viewModel.chosenAppetizer!)
                     .clipShape(RoundedRectangle(cornerRadius: 13))
                     .padding(.horizontal, 30)
                     .padding(.vertical, 100)
                     .shadow(radius: 15)
             }
+            
             if viewModel.isLoading {
                 ProgressView()
                     .tint(.brandPrimary)
             }
-            
-            
         }
+        
     }
 }
-
 #Preview {
     AppetizerListView()
 }
