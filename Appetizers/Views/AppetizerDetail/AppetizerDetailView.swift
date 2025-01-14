95c52ff2e1fd1e4d77b658fc9d23ba6ec40b1fdd
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-14 14:59:40 +0200

    Create AppetizerDetailView

---
+++ Appetizers/Views/AppetizerDetail/AppetizerDetailView.swift
@@ -0,0 +1,74 @@
+//
+//  AppetizerView.swift
+//  Appetizers
+//
+//  Created by Анастасія Грисюк on 13.01.2025.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack {
+            AppetizerImage(URLString: appetizer.imageURL)
+                .overlay(alignment: .topTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Text("X")
+                            .foregroundStyle(.black)
+                            .frame(width: 30, height: 30)
+                            .background(.white)
+                            .clipShape(Circle())
+                            .padding(10)
+                    }
+                }
+            
+            Text(appetizer.name)
+                .font(.title)
+                .fontWeight(.medium)
+                .padding(.horizontal, 20)
+            Text(appetizer.description)
+                .font(.title3)
+                .padding(20)
+            
+            HStack(spacing: 35) {
+                    NutrionView(name: "Calories", quantity: String(appetizer.calories))
+                    NutrionView(name: "Carbs", quantity: "\(appetizer.carbs) g")
+                    NutrionView(name: "Protein", quantity: "\(appetizer.protein) g")
+            }
+            .padding(.bottom, 40)
+            
+            OrderButton(text: "$\(appetizer.price) - Add To Order")
+                .padding(.horizontal, 20)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+struct NutrionView: View {
+    
+    var name: String
+    var quantity: String
+    
+    var body: some View {
+        VStack {
+            Text(name)
+                .fontWeight(.medium)
+                .padding(.bottom, 3)
+            Text(String(quantity))
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundStyle(.secondary)
+                .italic()
+        
+        }
+    }
+}
+
+#Preview {
+    AppetizerDetailView(appetizer: MockData.appetizer)
+}
---
+++ Appetizers/Views/Buttons/OrderButton.swift
@@ -0,0 +1,30 @@
+//
+//  OrderButton.swift
+//  Appetizers
+//
+//  Created by Анастасія Грисюк on 14.01.2025.
+//
+
+import SwiftUI
+
+struct OrderButton: View {
+    
+    let text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+            .foregroundStyle(.white)
+            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .background(.brandPrimary)
+            
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    OrderButton(text: "$10.00 - Add To Order")
+}
