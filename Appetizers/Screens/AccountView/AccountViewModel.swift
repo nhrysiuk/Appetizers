20192e60dc4c630d0555460cf63bf9fc19d7b753
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-15 16:01:37 +0200

    Creare Account View Model

--- Appetizers/Screens/AccountView/AccountViewModel.swift
+++ Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -5,4 +5,43 @@
 //  Created by Анастасія Грисюк on 15.01.2025.
 //
 
-import Foundation
+import SwiftUI
+
+class AccountViewModel: ObservableObject {
+    
+    @Published var firstName = String()
+    @Published var lastName = String()
+    @Published var emailString = String()
+    @Published var birthDate = Date()
+    @Published var needsExtraNapkins = Bool()
+    @Published var needsFrequentRefills = Bool()
+    
+    @Published var alertIsPresented = Bool()
+    @Published var alertItem: AlertItem?
+    
+    var isValidForm: Bool {
+        guard !firstName.isEmpty && !lastName.isEmpty && !emailString.isEmpty else {
+            alertItem = AlertContext.invalidForm
+            alertIsPresented = true
+            
+            return false
+        }
+
+        guard emailString.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            alertIsPresented = true
+
+            return false
+        }
+        
+        return true
+    }
+    
+    func saveChanges() {
+        guard isValidForm else {
+            return
+        }
+        
+        
+    }
+}
