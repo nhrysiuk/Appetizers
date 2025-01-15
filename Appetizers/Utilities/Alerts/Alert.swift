83394657c05a683506fdd3afcf791e82c6876be3
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-15 16:01:56 +0200

    Create Account alerts

--- Appetizers/Utilities/Alerts/Alert.swift
+++ Appetizers/Utilities/Alerts/Alert.swift
@@ -15,6 +15,7 @@ struct AlertItem: Identifiable {
 }
 
 struct AlertContext {
+    //MARK: - Network Alerts
     static let invalidData = AlertItem(title: "Server error",
                                        message: "The data received from the server was invalid. Please contact support",
                                        dismissButton: .default(Text("OK")))
@@ -22,4 +23,24 @@ struct AlertContext {
     static let invalidResponse = AlertItem(title: "Server error",
                                            message: "Invalid response from the server. Please contact support",
                                            dismissButton: .default(Text("OK")))
+    
+    //MARK: - Account Alerts
+    static let invalidForm = AlertItem(title: "Invalid Form",
+                                       message: "Please ensure all fields in the form have been filled out.",
+                                       dismissButton: .default(Text("OK")))
+    
+    static let invalidEmail = AlertItem(title: "Invalid Email",
+                                        message: "Please ensure your email is correct.",
+                                        dismissButton: .default(Text("OK"))
+    )
+    
+    static let userSaveSuccess = AlertItem(title: "Profile Saved",
+                                           message: "Your profile information was successfully saved.",
+                                           dismissButton: .default(Text("OK"))
+    )
+    
+    static let invalidUserData = AlertItem(title: "Profile Error",
+                                           message: "There was an error saving or retrieving your profile.",
+                                           dismissButton: .default(Text("OK"))
+    )
 }
