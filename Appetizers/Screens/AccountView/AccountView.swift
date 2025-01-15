3497edb72e850b60649c3a01b845c7b4930cea93
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-01-15 16:02:53 +0200

    Create Appetizer View UI and basic functionality

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @StateObject var viewModel = AccountViewModel()
+    
     var body: some View {
-        NavigationView {
-            Text("Account List View")
-                .navigationTitle("👤 Account")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("First Name", text: $viewModel.firstName)
+                    TextField("Last Name", text: $viewModel.lastName)
+                    TextField("Email", text: $viewModel.emailString)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $viewModel.birthDate,
+                               displayedComponents: .date)
+                    Button {
+                        viewModel.saveChanges()
+                    } label: {
+                        Text("Save Changes")
+                    }
+                } header: {
+                    Text("PERSONAL INFORMATION")
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $viewModel.needsExtraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.needsFrequentRefills)
+                }
+            }
+            .navigationTitle("👤 Account")
+        }
+        .alert(
+            viewModel.alertItem?.title ?? "аа",
+            isPresented: $viewModel.alertIsPresented
+        ) {
+        } message: {
+            Text(viewModel.alertItem?.message ?? "аа")
         }
     }
 }
+
 
 #Preview {
     AccountView()
