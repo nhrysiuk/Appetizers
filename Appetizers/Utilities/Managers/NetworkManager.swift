60a0d3617ea432709fd15d709d6a86085b027d38
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-08-31 23:27:35 +0300

    Implement fallback to local appetizer-data.json if API request fails

--- Appetizers/Utilities/Managers/NetworkManager.swift
+++ Appetizers/Utilities/Managers/NetworkManager.swift
@@ -21,8 +21,17 @@ final class NetworkManager {
             throw APIError.invalidURL
         }
         
-        let (data, _) = try await URLSession.shared.data(from: url)
+        let (data, response) = try await URLSession.shared.data(from: url)
         
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+              print("Network failed, fallback to local dataset.")
+              if let fallbackData = self.getAppetizersFallback() {
+                  print("Fallback data fetched successfully.")
+                  return fallbackData
+              } else {
+                  return []
+              }
+          }
         do {
             let decoder = JSONDecoder()
             return try decoder.decode(AppetizerResponse.self, from: data).request
@@ -32,7 +41,11 @@ final class NetworkManager {
         }
     }
     
-    func downloadImage(fromURLString: String) {
-        
+    func getAppetizersFallback() -> [Appetizer]? {
+        guard let url = Bundle.main.url(forResource: "appetizer-data", withExtension: "json"),
+              let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+        return try? JSONDecoder().decode([Appetizer].self, from: data)
     }
 }
